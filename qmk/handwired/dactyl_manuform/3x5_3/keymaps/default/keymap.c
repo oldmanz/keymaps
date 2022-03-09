6eaad2a051d451142e1b800286ffe0b0dd469d3f
Travis Zimmerman <oldmanz@pop-os.localdomain>
2022-03-09 17:59:06 -0500

    st

--- qmk/handwired/dactyl_manuform/3x5_3/keymaps/default/keymap.c
+++ qmk/handwired/dactyl_manuform/3x5_3/keymaps/default/keymap.c
@@ -3,6 +3,7 @@
 
 enum layers {
     BASE,
+    QWERTY,
     NAV,
     MOUSE,
     MEDIA,
@@ -17,11 +18,20 @@ enum layers {
 #define HOME_S LCTL_T(KC_S)
 #define HOME_T LSFT_T(KC_T)
 
+#define HOME_S2 LALT_T(KC_S)
+#define HOME_D LCTL_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
+
 // Right-hand home row mods
 #define HOME_N RSFT_T(KC_N)
 #define HOME_E RCTL_T(KC_E)
 #define HOME_I LALT_T(KC_I)
 #define HOME_O RGUI_T(KC_O)
+
+#define HOME_J RSFT_T(KC_J)
+#define HOME_K RCTL_T(KC_K)
+#define HOME_L LALT_T(KC_L)
+#define HOME_QU RGUI_T(KC_QUOT)
 
 // Alts
 #define ALT_X RALT_T(KC_X)
@@ -53,52 +63,60 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           		                 T_TAB,          T_ENT
   ),
 
+  [QWERTY] = LAYOUT_3x5_3(
+     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+     HOME_A,  HOME_S2, HOME_D,  HOME_F,  KC_G,           KC_H,    HOME_J,  HOME_K,  HOME_L,  HOME_QU,
+     KC_Z,    ALT_X,   KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_COMM, ALT_DO,  KC_SLSH,
+                      T_ESC,   T_SPACE,        T_BSPC,  T_DEL,
+                               T_TAB,          T_ENT
+  ),
+
   [NAV] = LAYOUT_3x5_3(
-     _______, _______, _______, _______, _______,        _______, _______, KC_UP,   _______, _______,
-     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,        KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,
-     _______, KC_RALT, _______, _______, _______,        KC_CAPS, KC_PGDN, _______, KC_PGUP, KC_INS,
-                        	_______, _______,        KC_BSPC, KC_DEL,
-					 _______,	 KC_ENT  
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX,
+     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,        KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,
+     XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX,        KC_CAPS, KC_PGDN, XXXXXXX, KC_PGUP, KC_INS,
+                        	XXXXXXX, XXXXXXX,        KC_BSPC, KC_DEL,
+					 XXXXXXX,	 KC_ENT  
   ),
   
   [MOUSE] = LAYOUT_3x5_3(
-     _______, _______, _______, _______, _______,        _______, _______, KC_MS_U, _______, _______,
-     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,        KC_WH_L, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R,
-     _______, KC_RALT, _______, _______, _______,        KC_CAPS, KC_WH_D, _______, KC_WH_U, _______,
-                        	_______, _______,        KC_BTN3, KC_BTN2,
-					 _______,	 KC_BTN1  
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, KC_MS_U, XXXXXXX, XXXXXXX,
+     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,        KC_WH_L, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R,
+     XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX,        KC_CAPS, KC_WH_D, XXXXXXX, KC_WH_U, XXXXXXX,
+                        	XXXXXXX, XXXXXXX,        KC_BTN3, KC_BTN2,
+					 XXXXXXX,	 KC_BTN1  
   ),
   
   [MEDIA] = LAYOUT_3x5_3(
-     _______, _______, _______, _______, _______,        _______, _______, KC_VOLU, _______, _______,
-     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,        _______, KC_MPRV, KC_VOLD, KC_MNXT, _______,
-     _______, KC_RALT, _______, _______, _______,        _______, KC_BRID, _______, KC_BRIU, _______,
-                        	_______, _______,        KC_MPLY, KC_MUTE,
-					 _______,	 KC_MSTP  
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, KC_VOLU, XXXXXXX, XXXXXXX,
+     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,        XXXXXXX, KC_MPRV, KC_VOLD, KC_MNXT, XXXXXXX,
+     XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, KC_BRID, XXXXXXX, KC_BRIU, XXXXXXX,
+                        	XXXXXXX, XXXXXXX,        KC_MPLY, KC_MUTE,
+					 XXXXXXX,	 KC_MSTP  
   ),
   
   [NUM] = LAYOUT_3x5_3(
-     KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC,        _______, _______, _______, _______, _______,
-     KC_SCLN, KC_4,    KC_5,    KC_6,    KC_EQL,         _______, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI,
-     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS,        _______, _______, _______, KC_RALT, _______,
-                                KC_DOT,  KC_0,           _______, _______,
-					 KC_MINS,	 _______  
+     KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     KC_SCLN, KC_4,    KC_5,    KC_6,    KC_EQL,         XXXXXXX, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI,
+     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS,        XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX,
+                                KC_DOT,  KC_0,           XXXXXXX, XXXXXXX,
+					 KC_MINS,	 XXXXXXX  
   ),
   
   [SYM] = LAYOUT_3x5_3(
-     KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,        _______, _______, _______, _______, _______,
-     KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,        _______, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI,
-     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE,        _______, _______, _______, KC_RALT, _______,
-                                KC_LPRN, KC_RPRN,        _______, _______,
-					 KC_UNDS,	 _______  
+     KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,        XXXXXXX, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI,
+     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE,        XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX,
+                                KC_LPRN, KC_RPRN,        XXXXXXX, XXXXXXX,
+					 KC_UNDS,	 XXXXXXX  
   ),
  
   [FUN] = LAYOUT_3x5_3(
-     KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR,        _______, _______, _______, _______, _______,
-     KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_SCRL,        _______, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI,
-     KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_PAUS,        _______, _______, _______, KC_RALT, _______,
-                                _______, KC_SPC,         _______, _______,
-					 KC_TAB,	 _______  
+     KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_SCRL,        XXXXXXX, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI,
+     KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_PAUS,        XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX,
+                                XXXXXXX, KC_SPC,         XXXXXXX, XXXXXXX,
+					 KC_TAB,	 XXXXXXX  
   ),
 };
 
@@ -110,13 +128,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 enum combo_events {
   CAPS_COMBO,
+  QWERTY_COMBO,
+  COLEMAK_COMBO,
   COMBO_LENGTH
 };
 uint16_t COMBO_LEN = COMBO_LENGTH;
 const uint16_t PROGMEM caps_combo[] = {KC_D, KC_H, COMBO_END};
+const uint16_t PROGMEM qwerty_combo[] = {HOME_A, T_SPACE, T_BSPC, HOME_O, COMBO_END};
+const uint16_t PROGMEM colemak_combo[] = {HOME_A, T_SPACE, T_BSPC, HOME_QU, COMBO_END};
 
 combo_t key_combos[] = {
     [CAPS_COMBO] = COMBO_ACTION(caps_combo),
+    [QWERTY_COMBO] = COMBO_ACTION(qwerty_combo),
+    [COLEMAK_COMBO] = COMBO_ACTION(colemak_combo),
 };
 
 void process_combo_event(uint16_t combo_index, bool pressed) {
@@ -127,5 +151,19 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
         caps_word_set(true);
       }
       break;
+
+    case QWERTY_COMBO:
+      if (pressed) {
+        layer_off(BASE);
+        layer_on(QWERTY);
+      }
+      break; 
+
+    case COLEMAK_COMBO:
+      if (pressed) {
+        layer_off(QWERTY);
+        layer_on(BASE);
+      }
+      break;    
   }
 }
